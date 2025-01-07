741b0e5d5236b68b1134d9f7559e3ea41435e22e
GlifeTimmy <trandinhtien19941@gmail.com>
2025-01-07 21:56:19 +0700

    get list category

---
+++ src/mongo/mongoConnecter.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+const uri = "mongodb://127.0.0.1:27017/"
+const dbName = "cd_backend"
+export default function mongoConnect() {
+    try {
+        mongoose.connect(`${uri}${dbName}`)
+        console.log("Connected to mongo successfully!")
+    } catch (error) {
+        console.log(error)
+        console.log("Connected to mongo failed!")
+    }
+
+}
---
+++ src/routers/categoryRoute.js
@@ -0,0 +1,11 @@
+import express from "express";
+import { listCategory } from  "../controllers/categoryController.js"
+const router = express.Router();
+
+
+router.get("/", listCategory)
+router.get("/create", function(req, res){
+    res.send("Create category")
+})
+
+export default router;
