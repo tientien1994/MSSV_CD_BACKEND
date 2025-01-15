d11f177347e2e88c88fecd3d0e5d6b5b8e0ec5fc
GlifeTimmy <trandinhtien19941@gmail.com>
2025-01-15 08:53:10 +0700

    sort category

--- src/controllers/categoryController.js
+++ src/controllers/categoryController.js
@@ -1,11 +1,19 @@
 import CategoryModel from "../models/categoryModel.js"
 import { ObjectId } from "mongodb"
 import { removeVietnameseAccents } from "../common/index.js"
+
+const sortObjects = [
+    {code: "name_DESC", name: "Tên giảm dần"},
+    {code: "name_ASC", name: "Tên tăng dần"},
+    {code: "code_DESC", name: "Mã giảm dần"},
+    {code: "code_ASC", name: "Mã tăng dần"},
+]
 export async function listCategory(req, res) {
     const search = req.query?.search
     const pageSize = !!req.query.pageSize ? parseInt(req.query.pageSize) : 5
     const page = !!req.query.page ? parseInt(req.query.page) : 1
     const skip = (page-1) * pageSize
+    const sort = !!req.query.sort ? req.query.sort : null
     let filters = {
         deletedAt: null
     }
@@ -15,13 +23,14 @@ export async function listCategory(req, res) {
     try {
         const countCategories = await CategoryModel.countDocuments(filters)
         const categories = await CategoryModel.find(filters).skip(skip).limit(pageSize)
-        console.log({page})
         res.render("pages/categories/list", {
             title: "Categories",
             categories: categories,
             countPagination: Math.ceil(countCategories/pageSize),
             pageSize,
             page,
+            sort,
+            sortObjects
         })
     } catch (error) {
         console.log(error)
